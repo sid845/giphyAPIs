5c8b26a9dd28b599b167d4a33b5afd89ddbef65e
unknown <siddharthpatel845@gmail.com>
2017-06-06 14:49:55 -0400

    initial commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 var buttons=["dog","cat","tiger","wolf","lion","deer","elephant","rat","monkey","hippo","lepord","eagle","hornbill","parrot"];
-function buttons()
+function animalButtons()
 {
   $("#animals").empty();
   for(var i=0;i<buttons.length;i++)
@@ -14,14 +14,15 @@ function buttons()
 $("#add-animal").on("click", function(event) {
   event.preventDefault();
   var animal =$("#animal-input").val().trim();
-  animals.push(animal);
-  buttons();
+  buttons.push(animal);
+  animalButtons();
 });
-buttons();
+animalButtons();
 $("button").on("click", function()
 {
   var animal = $(this).attr("data-name");
   var queryURL="http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=dc6zaTOxFJmzC";
+  console.log(queryURL);
   $.ajax({
       url: queryURL,
       method: "GET"
