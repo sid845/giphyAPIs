e32227d5e1b0093f77f74a915ad410068074d81b
unknown <siddharthpatel845@gmail.com>
2017-06-06 14:21:48 -0400

    initial commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,3 +18,25 @@ $("#add-animal").on("click", function(event) {
   buttons();
 });
 buttons();
+$("button").on("click", function()
+{
+  var animal = $(this).attr("data-name");
+  var queryURL="http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      console.log(response);
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var animalDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var animalImage = $("<img>");
+        animalImage.attr("src", results[i].images.fixed_height.url);
+        animalDiv.append(p);
+        animalDiv.append(animalImage);
+        $("#images").prepend(animalDiv);  
+    }
+
+}
